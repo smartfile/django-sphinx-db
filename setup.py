ee725a7bf81dda8d150dc26c5ba34d27d8aed50a
Ben Timby <btimby@gmail.com>
2012-09-25 11:53:10 -0400

    New release for new location.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 name = 'django_sphinx_db'
 version = '0.1'
-release = '2'
+release = '3'
 versrel = version + '-' + release
 readme = os.path.join(os.path.dirname(__file__), 'README.rst')
 download_url = 'https://github.com/downloads/smartfile/django-sphinx-db' \
